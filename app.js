ff0eb8cafd59b36c056d17fbb9acc6c0302bc7dc
Егор Вихарев <92789705+utkamag@users.noreply.github.com>
2022-02-10 19:02:34 +0400

    update code

---
+++ app.js
@@ -0,0 +1,78 @@
+// Минимальная оптимизация проекта, подгружаем JavaScript после загрузки DOM элементов
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Добавляем данные из input в div и создаем новые DOM элементы
+
+    function addTask() {
+        let task_input = document.getElementById("task_input").value
+
+        function localStorages() {
+
+        localStorage.setItem("data", task_input) // Local storage (тут, блин, не работает)
+        let localData = localStorage.getItem("data")
+        console.log(localData)
+        document.getElementById("list_text").innerHTML += localData
+        }
+        document.getElementById("task_input").value = ""
+
+        localStorages()
+    }
+
+    document.getElementById("task_add").addEventListener("click", addTask)
+
+
+
+
+    // Удаляем задачи
+
+    function deleteTasks() {
+        document.getElementById("task_delete").addEventListener("click", function () {
+            document.getElementById("list").remove()
+        })
+    }
+
+    deleteTasks()
+
+    // Все задачи выполнены!
+
+    function listImg() {
+        document.getElementById("task_ready").addEventListener("click", function () {
+            document.getElementById("list_img").src = "Img/true.png"
+        })
+    }
+
+    listImg()
+
+    // Задача выполнена
+
+    function listDone() {
+        document.getElementById("list_done").addEventListener("click", function () {
+            document.getElementById("list_img").src = "Img/true.png"
+        })
+    }
+
+    listDone()
+
+    // Задача не выполнена
+
+    function listNotDone() {
+        document.getElementById("list_not_done").addEventListener("click", function () {
+            document.getElementById("list_img").src = "Img/false.png"
+        })
+    }
+
+    listNotDone()
+
+    // Удалить задачу
+
+    function deleteElement() {
+        document.getElementById("list_delete").addEventListener("click", function () {
+            document.getElementById("list").remove()
+        })
+    }
+
+    deleteElement()
+
+});
+
+
